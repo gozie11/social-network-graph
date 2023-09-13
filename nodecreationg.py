cb0de8d30e94fbb8c42cedbac290376ff00e1611
gozie11 <gozieo7@gmail.com>
2023-09-12 20:23:10 -0500

    added functionality to feed data from excel

--- nodecreationg.py
+++ nodecreationg.py
@@ -1,9 +1,19 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import xlrd
+
+file = open("social network graph data.xlsx")
 
 G = nx.Graph()
 friends = []
 
+xlrd.open_workbook("social network graph data.xlsx")
+sheet = file.sheet_by_index(0)
+
+for row in range(sheet.nrows):
+    friends.append(((sheet.cell_value(row, 0), sheet.cell_value(row, 1))))
+
+print(friends)
 G.add_edges_from(friends)
 nx.draw(G, with_labels=True, node_color = "blue", font_size = 8, bbox=dict(facecolor='red', alpha=0.5), 
         node_size = 100, edge_color = "green", width = 2.0, style = "dashed", alpha = 0.5)
